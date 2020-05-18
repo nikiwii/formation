ab1eff01313b53dc1e8244e2a41612d9e46df562
nikiwii <hennette@gmail.com>
2020-05-18 23:17:00 +0200

    Communication par _GET

---
+++ jour7/communiquer/nav.php
@@ -0,0 +1,8 @@
+<?php
+$prexixe_nav = !empty($prexixe_nav) ? $prexixe_nav : "";
+?>
+
+<nav>
+    <a href="<?php echo $prexixe_nav ?>get.php">GET</a>
+    <a href="<?php echo $prexixe_nav ?>get_formulaire.php">Un formulaire (assez) complet</a>
+</nav>
